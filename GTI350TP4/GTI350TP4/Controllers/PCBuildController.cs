5764db13306a20a8bf837d2e0345687c00c67fd1
Digzol <digzol@hotmail.com>
2016-11-28 03:51:30 -0500

    Added PCBuild search

---
+++ GTI350TP4/GTI350TP4/Controllers/PCBuildController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GTI350TP4.Controllers
+{
+    public class PCBuildController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Search()
+		{
+			return View();
+		}
+
+		public ActionResult v()
+		{
+			return View();
+		}
+	}
+}
